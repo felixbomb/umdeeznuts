4cd2b806f8dc81c43fd7e1bff5d9752d5fdad6b0
Felix Baum <felixabaum@gmail.com>
2026-04-01 17:43:03 -0400

    first commit, starting dev process and creating api links (backend first)

---
+++ scripts/config.py
@@ -0,0 +1,8 @@
+ORIGIN = {"lat":38.991992832889466, "lon":-76.94501710253951}
+FAV_STOPS = [
+    "1321", #the y, outbound
+    "1322", #varsity, inbound
+    "1332", #cumberland
+    "1309", #la plata, inbound
+    "1001", #regents
+]
